11f8e8d1d9abcff070c72956a17cefd9d406c219
Stepan <stepan-pl@protonmail.com>
2022-09-20 00:16:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
+import logging
+import os
 from aiogram.utils import executor
-from create import dp
+from create import dp, TOKEN
 from handlers import client
 from threads import check_triggers, broadcast
 from aiogram import asyncio
 from triggers.triggerFinanz import TriggerFinanz
+from aiogram.utils.executor import start_webhook
 
 
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
-async def on_startup(_):
-    print('Бот онлайн')
+# webhook settings
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
+
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()    
 
 
+    
+    
 interval_broadcast = 20
 interval_triggers = 10
 
@@ -23,4 +41,17 @@ loop = asyncio.get_event_loop()
 loop.create_task(broadcast(interval_broadcast, trigger))
 loop.create_task(check_triggers(interval_triggers, trigger))
 
-executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+#executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+
+
+ if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
