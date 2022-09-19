a10d9eb02b7392557e5a4b00ed06b3dd15feb2b0
Stepan <stepan-pl@protonmail.com>
2022-09-20 00:51:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import logging
 import os
-from aiogram import Bot
-from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from create import dp, TOKEN
+from handlers import client
+from threads import check_triggers, broadcast
+from aiogram import asyncio
+from triggers.triggerFinanz import TriggerFinanz
 from aiogram.utils.executor import start_webhook
-from aiogram import Bot, types
 
 
-TOKEN = '5737568872:AAHNw4VyrniXNPx3mUZgbZojHKdHlicmOiA'
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
-
-HEROKU_APP_NAME = 'sgs-bot'
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
 # webhook settings
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
@@ -21,25 +20,35 @@ WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = os.getenv('PORT', default=8000)
 
-
 async def on_startup(dispatcher):
     await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
 
-
 async def on_shutdown(dispatcher):
-    await bot.delete_webhook()
+    await bot.delete_webhook()    
 
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(message.text)
+    
+    
+interval_broadcast = 20
+interval_triggers = 10
+
+trigger = TriggerFinanz()
+trigger.set_current_price()
+
+client.register_handlers(dp)
+
+loop = asyncio.get_event_loop()
+loop.create_task(broadcast(interval_broadcast, trigger))
+loop.create_task(check_triggers(interval_triggers, trigger))
+
+#executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
 
 
 if __name__ == '__main__':
    logging.basicConfig(level=logging.INFO)
    start_webhook(
        dispatcher=dp,
-       webhook_path=WEBHOOK_PATH, 
+       webhook_path=WEBHOOK_PATH,
        skip_updates=True,
        on_startup=on_startup,
        on_shutdown=on_shutdown,
