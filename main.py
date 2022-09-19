a5f7e9537c3c5ec289eae02aae070d7299136b5f
Stepan <stepan-pl@protonmail.com>
2022-09-20 00:44:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from aiogram.utils.executor import start_webhook
 from aiogram import Bot, types
 
 
-TOKEN = os.getenv('BOT_TOKEN')
+TOKEN = '5737568872:AAHNw4VyrniXNPx3mUZgbZojHKdHlicmOiA'
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+HEROKU_APP_NAME = 'sgs-bot'
 
 # webhook settings
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
