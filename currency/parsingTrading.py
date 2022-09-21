90fdb8ed5d038b899a21ea5952ad6446d09c2def
Stepan <stepan-pl@protonmail.com>
2022-09-21 20:08:45 +0300

    Update parsingTrading.py

--- currency/parsingTrading.py
+++ currency/parsingTrading.py
@@ -16,10 +16,8 @@ class ParsingTrading:
     }
 
     CORE_CODES = {
-        'Золото': "pid-8830-last",
-        'Палладий': "pid-8883-last",
-        'Платина': "pid-8910-last",
-        'Серебро': "pid-8836-last"
+        'Core Inflation Rate': 20,
+        'Food Inflation': 40
     }
 
     FOOD_CODES = {
@@ -43,11 +41,11 @@ class ParsingTrading:
 
     def get_metals_price(self):
         results_curr = {}
-        extra_url = 'russia/currency'
+        extra_url = 'russia/core-inflation-rate'
         full_page = requests.get(self.BASE_URL + extra_url, headers=self.headers)
         soup = bs(full_page.content, "html.parser")
-        for currency_code in self.CURRENCY_CODES.keys():
-            element_curr = soup.find_all("table", {"class": "table table-hover sortable-theme-minimal table-heatmap"})[1].find_all("td", {"id": "p"})[self.CURRENCY_CODES.get(currency_code)]
+        for currency_code in self.CORE_CODES.keys():
+            element_curr = soup.find_all("table", {"class": "table table-hover"})[0].find_all("td")[self.CORE_CODES.get(currency_code)]
             total_price = element_curr.text.replace("Т", "").replace(" ", "").replace(",", ".")
             print(currency_code + ": " + total_price)
             results_curr[currency_code] = total_price
