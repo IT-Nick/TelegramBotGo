ae7b872e7a52b00f0a3d6c1118e7bcccde1bb655
Stepan <stepan-pl@protonmail.com>
2022-09-21 15:59:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from threads import check_triggers, broadcast, broadcastInvesting, broadcastTrad
 from aiogram import asyncio
 from triggers.triggerFinanz import TriggerFinanz
 from triggers.triggerInvesting import TriggerInvesting
+from triggers.triggerTrading import TriggerTrading
 from aiogram.utils.executor import start_webhook
 
 
@@ -24,16 +25,19 @@ WEBAPP_PORT = os.getenv('PORT', default=8000)
 async def on_startup(dispatcher):
    interval_broadcast = 20
    interval_triggers = 10
+   interval_Trade = 25
    interval_investing = 30
    trigger = TriggerFinanz()
    trigger.set_current_price()
    triggerInv = TriggerInvesting()
    triggerInv.set_current_price()
+   triggerTrd = TriggerTrading()
+   triggerTrd.set_current_price()
    client.register_handlers(dp)
    loop = asyncio.get_event_loop()
    loop.create_task(broadcast(interval_broadcast, trigger))
    loop.create_task(broadcastInvesting(interval_investing, triggerInv))
-   loop.create_task(broadcastTrade(interval_investing, triggerInv))
+   loop.create_task(broadcastTrade(interval_Trade, triggerTrd))
    loop.create_task(check_triggers(interval_triggers, trigger, triggerInv))
    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
 
